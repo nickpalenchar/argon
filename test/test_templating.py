a55a9009789d5183cb81afa95328ef7bc3babfb9
Nick Palenchar <nickpal@nickpalenchar.com>
2020-12-09 20:57:18 -0500

    more unittests

--- test/test_templating.py
+++ test/test_templating.py
@@ -19,9 +19,23 @@ class TestArgon(unittest.TestCase):
             self.assertEqual(cat_file(os.path.join(workdir.name, 'hola.txt')),
                              """The giant says: fee, fi, fo, fum!""")
 
+    def test_renders_value_in_many_files(self):
+        with test_environment() as workdir:
+            subprocess.run(('argon', 'new', 'onevalmanyfiles', workdir.name, '--values',
+                            'global=Nick,alias=my_precious'))
+
+            tests = (('file1.txt', 'This line, also known as my_precious, ends with Nick.'),
+                     ('file2.txt', 'This line has Nick in the middle.'),
+                     ('file3.txt', 'Nick is at the start of this line.'))
+
+            for filename, expect in tests:
+                with self.subTest(key=filename):
+                    self.assertEqual(cat_file(os.path.join(workdir.name, filename)), expect)
+
 
     def test_renders_value_in_directory(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
